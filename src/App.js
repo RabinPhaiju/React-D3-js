ab722c7b3893439156c2aa92a15999467e24f85d
Rabin Phaiju <rabinphaiju15@gmail.com>
2022-06-29 11:22:20 +0545

    added stacked bar chart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React,{useRef, useState} from 'react'
+import React,{ useState} from 'react'
 import BarChart3 from './charts/BarChart3'
+import StackedBarChart from "./charts/StackedBarChart";
 
 function App() {
-
-  const ref = useRef(null)
   const [width,setWidth] = useState(0)
 
   const resizeWidth = ()=>{
@@ -18,20 +17,37 @@ function App() {
   window.addEventListener('resize',resizeWidth)
 
   const data = [
-    {label:'Su',value:1.11},
-    {label:'Mo',value:1.59},
-    {label:'Ma',value:1.25},
-    {label:'Me',value:1.46},
-    {label:'Ju',value:0.75},
-    {label:'Ve',value:1.00},
-    {label:'Sa',value:1.66},
-    {label:'Test',value:0.44},
+    {label:'Weight',value:4},
+    {label:'Moon',value:3.},
+    {label:'Mars',value:5},
+    {label:'Mercury',value:2},
+    {label:'Jupiter',value:3},
+    {label:'Venus',value:0},
+    {label:'Saturn',value:4.9},
+    {label:'Rahu',value:1.35},
+    {label:'Ketu',value:3.5},
   ]
+  const stackData = [
+    {label: 'Weight',"D1": 5,"D2": 2,"D3": 2,"D4": 2,"D9": 4,"D10": 3,"D12": 1,"D30": 1,"Total":20},
+    {label: 'Sun',"D1": 0.5,"D2": 1.8,"D3": 0.7,"D4": 1.5,"D9": 0.4,"D10": 1.05,"D12": 0.75,"D30": 0.75,"Total":7.45},
+    {label: 'Moon',"D1": 3,"D2": 0,"D3": 0.7,"D4": 1.2,"D9": 2.4,"D10": 1.8,"D12": 0.35,"D30": 0.75,"Total":10.2},
+    {label: 'Mars',"D1": 2.5,"D2": 1.5,"D3": 1.8,"D4": 1,"D9": 2.4,"D10": 2.25,"D12": 0.5,"D30": 0.6,"Total":12.55},
+    {label: 'Mercury',"D1": 2.5,"D2": 1,"D3": 1.8,"D4": 1,"D9": 2,"D10": 1.5,"D12": 0.5,"D30": 0.6,"Total":10.9},
+    {label: 'Jupiter',"D1": 3.75,"D2": 1.5,"D3": 2,"D4": 1,"D9": 2,"D10": 3,"D12": 0.9,"D30": 1,"Total":15.15},
+    {label: 'Venus',"D1": 0,"D2": 1,"D3": 1,"D4": 0.2,"D9": 2.4,"D10": 1.8,"D12": 0.5,"D30": 0.5,"Total":7.4},
+    {label: 'Saturn',"D1": 4.5,"D2": 0.2,"D3": 1,"D4": 1,"D9": 0.4,"D10": 1.5,"D12": 0.5,"D30": 0.9,"Total":10},
+    {label: 'Rahu',"D1": 1.75,"D2": 0.2,"D3": 0.7,"D4": 0.7,"D9": 2,"D10": 2.25,"D12": 0.35,"D30": 0.75,"Total":8.7},
+    {label: 'Ketu',"D1": 3,"D2": 1,"D3": 1.2,"D4": 1.2,"D9": 2.4,"D10": 1.5,"D12": 0.6,"D30": 0.5,"Total":11.4},
+  ];
+  const stackKeys = ['D1','D2','D3','D4','D9','D10','D12','D30'];
+  const stackColors = {'D1':'red','D2':'purple','D3':'green','D4':'blue','D9':'skyblue','D10':'yellow','D12':'gray','D30':'black'};
 
   return (
-    <div className="App" ref={ref}>
-    <div>This is Bar chart</div>
-        <BarChart3 w={width} data={data} barColor="purple" minValue={1} />
+    <div className="App">
+    {/* <div>This is Bar chart</div> */}
+        {/* <BarChart3 w={width} data={data}  barColor="purple" minValue={1} /> */}
+      {/* <div>Stack bar chart</div> */}
+      <StackedBarChart w={width} data={stackData}  keys={stackKeys} colors={stackColors} barColor="purple" minValue={1}/>
     </div>
   )
 }
---
+++ src/charts/StackedBarChart.js
@@ -0,0 +1,197 @@
+import { useEffect,useState, useRef,Fragment } from "react"
+import './barChart.css'
+import {
+  axisBottom,
+  axisLeft,
+  scaleBand,
+  scaleLinear,
+  stack,
+  stackOrderAscending,
+  select
+} from "d3"
+
+function Bar({
+  x,
+  y,
+  width,
+  height,
+  color,
+  onMouseEnter,
+  onMouseLeave
+}) {
+  const radius = height === 0 ? 0 : width * 0.2
+  // https://stackoverflow.com/questions/10177985/svg-rounded-corner
+  return (
+      <path
+      d={`
+      m${x},${y + radius}
+      a${radius},${radius} 0 0 1 ${radius},${-radius}
+      h${width - radius*2}      
+      a${radius},${radius} 0 0 1 ${radius},${radius}
+      v${height - radius*2}
+      a${radius},${radius} 0 0 1 ${-radius},${radius}
+      h-${width - radius*2}
+      a${radius},${radius} 0 0 1 ${-radius},${-radius}
+      z
+    `}
+      fill={color}
+      onMouseEnter={(event) => onMouseEnter(event)}
+      onMouseLeave={onMouseLeave}
+    />
+  )
+}
+
+
+function AxisBottom({ scale, width, transform }) {
+  const ref = useRef (null)
+
+  useEffect(() => {
+    let xAxisGen = axisBottom(scale)
+    if (ref.current) {
+      select(ref.current).call(xAxisGen)
+      select(ref.current).attr('font-size', '13px').attr('stroke-width', 0)
+      .attr('font-weight', '600')
+      // .attr("color", "black")
+    }
+  }, [scale])
+
+  return <g ref={ref} transform={transform} >
+    <path stroke="gray" strokeWidth={'3px'} d={`M 0 0 h ${width/1.05}`} ></path>
+  </g>
+}
+function AxisMinimum({ scale, width, transform }) {
+  const ref = useRef (null)
+  useEffect(() => {
+    let xAxisGen = axisBottom(scale)
+    if (ref.current) {
+      select(ref.current).call(xAxisGen)
+      select(ref.current).attr('font-size', '0px').attr('stroke-width',0)
+    }
+  }, [scale])
+  return <g ref={ref} transform={transform} >
+    <path stroke="gray" strokeWidth={'0.5px'} d={`M 0 6 h ${width/1.05}`} ></path> 
+  </g>
+}
+
+function AxisLeft({ scale, height }) {
+  const ref = useRef (null)
+  useEffect(() => {
+    if (ref.current) {
+      select(ref.current).call(axisLeft(scale))
+      select(ref.current).attr('font-size', '10px').attr('stroke-width', 0)
+    }
+  }, [scale])
+  return <g ref={ref} >
+    <path stroke="gray" strokeWidth={'3px'} d={`M 0 0 v ${height}`} ></path>
+  </g>
+}
+
+function Bars({ data, height, scaleX, scaleY, setTooltip,margin,barColor }) {
+  return (
+    <>
+      {data.map(({ value, label }) => (
+        <Fragment key={`bar-${label}`}>
+        <Bar
+          x={scaleX(label)}
+          y={scaleY(value)}
+          width={scaleX.bandwidth()}
+          height={height - scaleY(value)}
+            color={barColor}
+          onMouseEnter={(event) => {
+            setTooltip({
+              x: event.clientX,
+              y: event.clientY,
+              index: label,
+               value: value
+            })
+          }}
+        onMouseLeave={() => setTooltip(null)}
+        />
+          <text
+            transform={`translate(${scaleX.bandwidth() + scaleX(label) - (scaleX.bandwidth() / 2)}, ${scaleY(value) - 8})`}
+            textAnchor="middle"
+            alignmentBaseline="baseline"
+            fill="black"
+            fontSize={(scaleX.bandwidth()/2)+'px'}
+            fontWeight="700"
+          >
+            {value}
+          </text>
+        </Fragment>
+      ))}
+    </>
+    
+  )
+}
+function StackedChart({ data,keys, height, scaleX, scaleY, setTooltip,margin,colors,barColor }) {
+  const ref = useRef();
+
+  useEffect(() => {
+    if(ref.current){
+    const stackGenerator = stack().keys(keys)
+    // .order(stackOrderAscending);
+    const layers = stackGenerator(data);
+
+    select(ref.current)
+      .selectAll(".layer")
+      .data(layers)
+      .join("g")
+      .attr("class", "layer")
+      .attr("fill", layer => colors[layer.key])
+      .selectAll("rect")
+      .data(layer => layer)
+      .join("rect")
+      .attr("x", sequence => scaleX(sequence.data.label))
+      .attr("width", scaleX.bandwidth())
+      .attr("y", sequence => scaleY(sequence[1]))
+      .attr("height", sequence => scaleY(sequence[0]) - scaleY(sequence[1]));
+      // .attr("height", 10);
+    }
+  }, [scaleX,scaleY,colors, data, keys]);
+
+  return (
+      <g ref={ref}></g>
+  );
+}
+
+export default function StackedBarChart({w,data,keys,colors,barColor,minValue }) {
+  let h = w/1.5;
+  const [tooltip, setTooltip] = useState(null)
+  const margin = { top: 20, right: 20, bottom: 20, left: 20 }
+  const width = w - margin.left - margin.right
+  const height = h - margin.top - margin.bottom
+
+  const scaleX = scaleBand()
+    .domain(data.map(({ label }) => label))
+    // .range([0, width])
+    .range([0, width - margin.left - margin.right])
+    .padding(0.5)
+
+  const scaleY = scaleLinear()
+    // .domain([0, Math.max(...data.map(({ value }) => value.reduce((a,b)=>a+b,0)))*1.12])
+    .domain([0, 25])
+    
+    .range([height, 0])
+
+  return (
+    <div style={{backgroundColor:'lightgray',width:width,overflow:'hidden'}}>
+    <svg
+      width={width + margin.left + margin.right}
+      height={height + margin.top + margin.bottom}
+    >
+      <g transform={`translate(${margin.left}, ${margin.top})`}>
+          <AxisBottom scale={scaleX} width={width} transform={`translate(0, ${height})`} />
+          <AxisMinimum scale={scaleX} width = {width}  transform={`translate(0, ${scaleY(minValue)-7})`} />
+          <AxisLeft scale={scaleY} height={height} />
+          {/* <Bars data={data} height={height} scaleX={scaleX} scaleY={scaleY} setTooltip={setTooltip} margin={margin} barColor={barColor} /> */}
+          <StackedChart data={data} keys={keys} height={height} scaleX={scaleX} scaleY={scaleY} setTooltip={setTooltip} margin={margin} colors={colors} barColor={barColor} />
+      </g>
+    </svg>
+      {tooltip !== null ? (
+        <div className="tooltip" style={{ top: tooltip.y, left: tooltip.x }}>
+          <span>{tooltip.index} {tooltip.value}</span>
+        </div>
+      ) : null}
+    </div>
+  )
+}
